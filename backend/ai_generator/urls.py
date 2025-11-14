c20f423705e9a38e025c4dd6565134a50fdd799c
202301473 <202301473@dau.ac.in>
2025-11-14 19:40:56 +0530

    Clear all files except README.md

--- backend/ai_generator/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('chat/', views.chat, name='chat'),
-    path('download-pdf/', views.download_pdf, name='download_pdf'),
-    path('preview/', views.preview_document, name='preview_document'),
-]

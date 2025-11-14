c2f1e657a621ff8f049ee20308930c95c5addff8
SharnamShah01 <202301247@dau.ac.in>
2025-11-14 14:25:40 +0530

    Langchain updated
    Langchain updated - sharnam shah

--- backend/ai_generator/urls.py
+++ backend/ai_generator/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('chat/', views.chat, name='chat'),
+    path('download-pdf/', views.download_pdf, name='download_pdf'),
+    path('preview/', views.preview_document, name='preview_document'),
 ]
